916f75be613954bba9e65564d8c69af9b49784a8
Patrick Smith <smithpm@usc.edu>
2020-06-26 18:18:11 -0700

    Initial Commit

---
+++ StoryBoard1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoard1
+//
+//  Created by Jenna Smith on 6/26/20.
+//  Copyright © 2020 Unmasked Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoryBoard1Tests/StoryBoard1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  StoryBoard1Tests.swift
+//  StoryBoard1Tests
+//
+//  Created by Jenna Smith on 6/26/20.
+//  Copyright © 2020 Unmasked Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import StoryBoard1
+
+class StoryBoard1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

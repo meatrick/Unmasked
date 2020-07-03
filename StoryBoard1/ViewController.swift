99e5f7c44a6b76a5440b19c74d038c922b439af3
Patrick Smith <smithpm@usc.edu>
2020-07-03 14:45:42 -0700

    Added Autocomplete button, and it functionally brings up a new modal to search.  The button is not placed properly

--- StoryBoard1/ViewController.swift
+++ StoryBoard1/ViewController.swift
@@ -73,8 +73,11 @@ class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDel
         
         placesClient = GMSPlacesClient.shared()
         
+        // search bar button
+        makeButton()
 }
 
+    
     override func loadView() {
       let camera = GMSCameraPosition.camera(withLatitude: 47.603,
                                             longitude:-122.331,
@@ -146,6 +149,69 @@ class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDel
         locationManager.stopUpdatingLocation()
         print("Error: \(error)")
       }
+
+    
+    
+    // MARK: Places API
+    
+    // Present the Autocomplete view controller when the button is pressed.
+    @objc func autocompleteClicked(_ sender: UIButton) {
+      let autocompleteController = GMSAutocompleteViewController()
+      autocompleteController.delegate = self
+
+      // Specify the place data types to return.
+      let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+        UInt(GMSPlaceField.placeID.rawValue))!
+      autocompleteController.placeFields = fields
+
+      // Specify a filter.
+      let filter = GMSAutocompleteFilter()
+      filter.type = .address
+      autocompleteController.autocompleteFilter = filter
+
+      // Display the autocomplete view controller.
+      present(autocompleteController, animated: true, completion: nil)
+    }
+
+    // Add a button to the view.
+    func makeButton() {
+        let btnLaunchAc = UIButton(frame: CGRect(x: 5, y: 5, width: 300, height: 35))
+        btnLaunchAc.backgroundColor = .blue
+        btnLaunchAc.setTitle("Search...", for: .normal)
+        btnLaunchAc.addTarget(self, action: #selector(autocompleteClicked), for: .touchUpInside)
+        self.view.addSubview(btnLaunchAc)
+    }
+    
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place ID: \(place.placeID)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
 
 }
 
